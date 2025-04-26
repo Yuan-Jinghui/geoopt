e541e0dd48d1a8fb94d83a16716b9016ef9f881c
Yuan-Jinghui <132837003+Yuan-Jinghui@users.noreply.github.com>
2025-04-26 11:13:53 +0800

    Update __init__.py

--- geoopt/optim/__init__.py
+++ geoopt/optim/__init__.py
@@ -1,5 +1,6 @@
 from .rsgd import RiemannianSGD
 from .radam import RiemannianAdam
+from .radan import RiemannianAdan
 from .sparse_radam import SparseRiemannianAdam
 from .sparse_rsgd import SparseRiemannianSGD
 from .rlinesearch import RiemannianLineSearch
